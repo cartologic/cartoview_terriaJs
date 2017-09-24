f51ede47e1afa82f9f054990f416b141d00520f9
hisham waleed karam <hisham.karam@cartologic.com>
2017-09-24 10:29:00 +0200

    auto populate with cartoview maps

--- views.py
+++ views.py
@@ -1,61 +1,49 @@
+# -*- coding: utf-8 -*-
 import json
 
-from django.shortcuts import HttpResponse, render, render_to_response
+from django.shortcuts import HttpResponse
+from geonode.maps.models import Map
+from guardian.shortcuts import get_objects_for_user
+from django.conf import settings
+geoserver_url = settings.OGC_SERVER['default']['PUBLIC_LOCATION']
+config = {
+    'allowProxyFor': ["nicta.com.au", "gov.au", "csiro.au",
+                      "arcgis.com",
+                      "argo.jcommops.org",
+                      "www.abc.net.au",
+                      "geoserver.aurin.org.au",
+                      "mapsengine.google.com",
+                      "s3-ap-southeast-2.amazonaws.com",
+                      "adelaidecitycouncil.com",
+                      "www.dptiapps.com.au",
+                      "geoserver-123.aodn.org.au",
+                      "geoserver.imos.org.au",
+                      "nci.org.au",
+                      "static.nationalmap.nicta.com.au",
+                      "githubusercontent.com",
+                      "gov",
+                      "gov.uk",
+                      "gov.nz"
+                      ],
+    'proxyAllDomains': False
+}
 
 
 def server_config(request):
-    config = {
-        'allowProxyFor': ["nicta.com.au", "gov.au", "csiro.au",
-                          "arcgis.com",
-                          "argo.jcommops.org",
-                          "www.abc.net.au",
-                          "geoserver.aurin.org.au",
-                          "mapsengine.google.com",
-                          "s3-ap-southeast-2.amazonaws.com",
-                          "adelaidecitycouncil.com",
-                          "www.dptiapps.com.au",
-                          "geoserver-123.aodn.org.au",
-                          "geoserver.imos.org.au",
-                          "nci.org.au",
-                          "static.nationalmap.nicta.com.au",
-                          "githubusercontent.com",
-                          "gov",
-                          "gov.uk",
-                          "gov.nz"
-                          ],
-        'proxyAllDomains': False,
-        'version': "2.6.7"
-    }
-    return HttpResponse(content=json.dumps(config),
+    server_config_dict = config.copy()
+    server_config_dict.update({'version': "2.6.7"})
+    return HttpResponse(content=json.dumps(server_config_dict),
                         content_type='application/json')
 
 
 def proxyabledomains(request):
-    config = {
-        'allowProxyFor': ["nicta.com.au", "gov.au", "csiro.au",
-                          "arcgis.com",
-                          "argo.jcommops.org",
-                          "www.abc.net.au",
-                          "geoserver.aurin.org.au",
-                          "mapsengine.google.com",
-                          "s3-ap-southeast-2.amazonaws.com",
-                          "adelaidecitycouncil.com",
-                          "www.dptiapps.com.au",
-                          "geoserver-123.aodn.org.au",
-                          "geoserver.imos.org.au",
-                          "nci.org.au",
-                          "static.nationalmap.nicta.com.au",
-                          "githubusercontent.com",
-                          "gov",
-                          "gov.uk",
-                          "gov.nz"
-                          ],
-        'proxyAllDomains': False
-    }
-    return HttpResponse(content=json.dumps(config), content_type='application/json')
+    return HttpResponse(content=json.dumps(config),
+                        content_type='application/json')
 
 
 def terria_json(request):
+    permitted_ids = get_objects_for_user(request.user,
+                                         'base.view_resourcebase').values('id')
     config = {
         "homeCamera": {
             "north": -8,
@@ -63,46 +51,34 @@ def terria_json(request):
             "south": -45,
             "west": 109
         },
-        "catalog": [{
-                    "name": "Cartoview Demo",
-                    "type": "group",
-                    "preserveOrder": True,
-                    "isOpen": True,
-                    "items": [{
-                        "name": "Cartoview",
-                        "type": "group",
-                        "isOpen": True,
-                        "items": [{
-                            "name": "All WFS Dataset",
-                            "type": "wfs-getCapabilities",
-                            "url": "http://try.cartoview.net/geoserver/wfs"
-                        },
-                            {
-                            "name": "nyc_fatality_yearly",
-                            "url": "http://try.cartoview.net/geoserver/gwc/service/wms",
-                            "type": "wms",
-                            "layers": "geonode:nyc_fatality_yearly",
-                            "isGeoServer": True,
-                            "availableStyles": [
-                                {
-                                    "name": "nyc_fatality_yearly_1502620816915",
-                                    "title": "style123",
-                                    "legendUrl": "http://try.cartoview.net/geoserver/ows?service=WMS&request=GetLegendGraphic&format=image%2Fpng&width=20&height=20&layer=geonode%3Anyc_fatality_yearly&style=nyc_fatality_yearly_1502620816915"
-                                }
-                            ]
-                        },
-                            {
-                            "name": "GeoWebCache WMS (fast)",
-                            "url": "http://try.cartoview.net/geoserver/gwc/service/wms",
-                            "type": "wms-getCapabilities"
-                        },
-                            {
-                            "name": "Normal WMS (slow)",
-                            "url": "http://try.cartoview.net/geoserver/wms",
-                            "type": "wms-getCapabilities"
-                        }
-                        ]
-                    }]
-                    }]
     }
-    return HttpResponse(content=json.dumps(config), content_type='application/json')
+    maps_catalog = {
+        "name": "Cartoview Maps",
+        "type": "group",
+        "preserveOrder": True,
+        "isOpen": True}
+    catalog = []
+    maps = Map.objects.filter(id__in=permitted_ids)
+    for map in maps:
+        map_item = {
+            "name": map.title,
+            "type": "group",
+            "isOpen": False
+        }
+        layers = []
+        for layer in map.local_layers:
+            layer_item = {
+                "name": layer.title,
+                "url": "{}{}".format(geoserver_url, 'wms'),
+                "description": layer.abstract,
+                "type": "wms",
+                "isGeoServer": True,
+                "layers": layer.typename
+            }
+            layers.append(layer_item)
+        map_item.update({"items": layers})
+        catalog.append(map_item)
+    maps_catalog.update({"items": catalog})
+    config.update({"catalog": [maps_catalog]})
+    return HttpResponse(content=json.dumps(config),
+                        content_type='application/json')

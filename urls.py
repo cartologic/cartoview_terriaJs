57bf2940a1710bec486f8ad11528399bee618a94
hisham waleed karam <hisham.karam@cartologic.com>
2017-09-26 11:48:06 +0200

    organize code to be easy to maintain

--- urls.py
+++ urls.py
@@ -1,17 +1,2 @@
-from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
-from .views import server_config, proxyabledomains, terria_json, index, map_list
-from . import APP_NAME, views
-
-urlpatterns = patterns('',
-                       url(r'^$', map_list,
-                           name='%s.index' % APP_NAME),
-                       url(r'^(?P<map_id>\d+)$', index,
-                           name='%s.list' % APP_NAME),
-                       url(r'^serverconfig/$', server_config,
-                           name='%s.config' % APP_NAME),
-                       url(r'^proxyabledomains/$', server_config,
-                           name='%s.proxy' % APP_NAME),
-                       url(r'^init/terria.json$', terria_json,
-                           name='%s.json' % APP_NAME)
-                       )
+from .views import CARTOVIEW_TERRIA
+urlpatterns = CARTOVIEW_TERRIA.get_urls_patterns()
--- views.py
+++ views.py
@@ -2,14 +2,19 @@
 import json
 
 from django.conf import settings
+from django.conf.urls import patterns, url
 from django.shortcuts import HttpResponse, render
 from geonode.maps.models import Map
 from guardian.shortcuts import get_objects_for_user
 from pyproj import Proj, transform
 
-geoserver_url = settings.OGC_SERVER['default']['PUBLIC_LOCATION']
-terria_map = "terria_map_id"
-config = {
+from . import APP_NAME
+
+_templates = {
+    "terria_template": "cartoview_terriaJs/terria.html",
+    "map_list_template": "cartoview_terriaJs/list.html"
+}
+_config = {
     'allowProxyFor': ["nicta.com.au", "gov.au", "csiro.au",
                       "arcgis.com",
                       "argo.jcommops.org",
@@ -32,87 +37,125 @@ config = {
 }
 
 
-def reproject(x1, y1):
-    geonode_projection = settings.DEFAULT_MAP_CRS
-    map_projection = "EPSG:3785".lower() if int(geonode_projection.split(":")
-                                                [1]) == 900913 else geonode_projection
+class CartoviewTerriaMap(object):
+    def __init__(self, templates=_templates, config=_config):
+        self.terria_template = _templates.get('terria_template', None)
+        self.map_list_template = _templates.get('map_list_template', None)
+        self.geoserver_url = settings.OGC_SERVER['default']['PUBLIC_LOCATION']
+        self.terria_map = "terria_map_id"
+        self.geonode_projection = settings.DEFAULT_MAP_CRS
+        self.map_projection = "EPSG:3785".lower() if int(
+            self.geonode_projection.split(":")[1]) == 900913 else \
+            self.geonode_projection
+        self.in_proj = Proj(init=self.map_projection, preserve_units=True)
+        self.out_proj = Proj(init='epsg:4326')
+        self.main_config = _config
+        self.server_config = self.main_config.copy()
+        self.server_config.update(
+            {'version': "2.6.7"})
 
-    in_proj = Proj(init=map_projection, preserve_units=True)
-    out_proj = Proj(init='epsg:4326')
-    x2, y2 = transform(in_proj, out_proj, x1, y1)
-    return (x2, y2)
+    def reproject(self, x1, y1):
+        """reproject Geonode maps center to terria 
+          :param x1: map.center_x
+          :param y1: map.center_y
+          :type x1: type int
+          :type y1: type int
+          :return: return projected points
+          :rtype:  tuple(int,int)
+        """
+        x2, y2 = transform(self.in_proj, self.out_proj, x1, y1)
+        return (x2, y2)
 
+    def index_page(self, request, map_id):
+        template = self.terria_template
+        map_id_key = request.session.get(self.terria_map, None)
+        current_id = int(map_id_key) if map_id_key else 0
+        if not current_id or current_id != map_id:
+            request.session[self.terria_map] = map_id
+        return render(request, template, context={})
 
-def index(request, map_id):
-    template = "cartoview_terriaJs/terria.html"
-    map_id_key = request.session.get(terria_map, None)
-    current_id = int(map_id_key) if map_id_key else 0
-    if not current_id or current_id != map_id:
-        request.session[terria_map] = map_id
-    return render(request, template, context={})
+    def map_list(self, request):
+        template = self.map_list_template
+        return render(request, template, context={"site_url": settings.SITEURL})
 
+    def server_config_view(self, request):
+        return HttpResponse(content=json.dumps(self.server_config),
+                            content_type='application/json')
 
-def map_list(request):
-    template = "cartoview_terriaJs/list.html"
-    return render(request, template, context={"site_url": settings.SITEURL})
+    def proxyable_domains(self, request):
+        return HttpResponse(content=json.dumps(self.main_config),
+                            content_type='application/json')
 
-
-def server_config(request):
-    server_config_dict = config.copy()
-    server_config_dict.update({'version': "2.6.7"})
-    return HttpResponse(content=json.dumps(server_config_dict),
-                        content_type='application/json')
-
-
-def proxyabledomains(request):
-    return HttpResponse(content=json.dumps(config),
-                        content_type='application/json')
-
-
-def terria_json(request):
-    map_id = request.session.get(terria_map, None)
-    permitted_ids = get_objects_for_user(request.user,
-                                         'base.view_resourcebase').values('id')
-    config = {
-
-    }
-    maps_catalog = {
-        "name": "Cartoview Maps",
-        "type": "group",
-        "preserveOrder": True,
-        "isOpen": True}
-    catalog = []
-    maps = Map.objects.filter(id__in=permitted_ids)
-    for map in maps:
-        map_item = {
-            "name": map.title,
-            "type": "group",
-            "isOpen": False
-        }
+    def build_map_catalog(self, map, current_map_id):
         layers = []
         for layer in map.local_layers:
             layer_item = {
                 "name": layer.title,
-                "url": "{}{}".format(geoserver_url, 'wms'),
+                "url": "{}{}".format(self.geoserver_url, 'wms'),
                 "description": layer.abstract,
                 "type": "wms",
                 "isGeoServer": True,
                 "layers": layer.typename
             }
-            if map_id and int(map_id) == map.id:
+            if current_map_id and int(current_map_id) == map.id:
                 layer_item.update({"isShown": True, "isEnabled": True})
             layers.append(layer_item)
+        return layers
 
-        if map_id and int(map_id) == map.id:
-            x, y = reproject(map.center_x, map.center_y)
-            config.update({"homeCamera": {
-                "west": x,
-                "south": y,
-                "east": x,
-                "north": y}})
-        map_item.update({"items": layers})
-        catalog.append(map_item)
-    maps_catalog.update({"items": catalog})
-    config.update({"catalog": [maps_catalog]})
-    return HttpResponse(content=json.dumps(config),
-                        content_type='application/json')
+    def build_main_catalog(self, permitted_ids, current_map_id, config={}):
+        maps = Map.objects.filter(id__in=permitted_ids)
+        maps_catalog = {
+            "name": "Cartoview Maps",
+            "type": "group",
+            "preserveOrder": True,
+            "isOpen": True}
+        catalog = []
+        for map in maps:
+            map_item = {
+                "name": map.title,
+                "type": "group",
+                "isOpen": False
+            }
+            layers_as_catalog_item = self.build_map_catalog(
+                map, current_map_id)
+            if current_map_id and int(current_map_id) == map.id:
+                x, y = self.reproject(map.center_x, map.center_y)
+                config.update({"homeCamera": {
+                    "west": x,
+                    "south": y,
+                    "east": x,
+                    "north": y}})
+            map_item.update({"items": layers_as_catalog_item})
+            catalog.append(map_item)
+        maps_catalog.update({"items": catalog})
+        config.update({"catalog": [maps_catalog]})
+        return config
+
+    def terria_json(self, request):
+        map_id = request.session.get(self.terria_map, None)
+        permitted_ids = get_objects_for_user(
+            request.user, 'base.view_resourcebase').values('id')
+        catalog = self.build_main_catalog(permitted_ids, map_id)
+        return HttpResponse(content=json.dumps(catalog),
+                            content_type='application/json')
+
+    def get_urls_patterns(self):
+        url_patterns = patterns('',
+                                url(r'^$', self.map_list,
+                                    name='%s.index' % APP_NAME),
+                                url(r'^(?P<map_id>\d+)$', self.index_page,
+                                    name='%s.list' % APP_NAME),
+                                url(r'^serverconfig/$',
+                                    self.server_config_view,
+                                    name='%s.config' % APP_NAME),
+                                url(r'^proxyabledomains/$',
+                                    self.proxyable_domains,
+                                    name='%s.proxy' % APP_NAME),
+                                url(r'^init/terria.json$', self.terria_json,
+                                    name='%s.json' % APP_NAME)
+
+                                )
+        return url_patterns
+
+
+CARTOVIEW_TERRIA = CartoviewTerriaMap()

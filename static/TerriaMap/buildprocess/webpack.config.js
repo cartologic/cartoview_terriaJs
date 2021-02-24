ed985bc85eab2813ccba8bda0d7f16a4cc70bddc
Mohamed Radwan <mohamed.radwan@cartologic.com>
2021-02-24 14:15:57 +0200

    Fix wrong call for static files after build

--- static/TerriaMap/buildprocess/webpack.config.js
+++ static/TerriaMap/buildprocess/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = function(devMode, hot) {
             path: path.resolve(__dirname, '..', 'wwwroot', 'build'),
             filename: 'TerriaMap.js',
             // work around chrome needing the full URL when using sourcemaps (http://stackoverflow.com/questions/34133808/webpack-ots-parsing-error-loading-fonts/34133809#34133809)
-            publicPath: hot ? 'http://localhost:3003/build/' : 'build/',
+            publicPath: hot ? 'http://localhost:3003/build/' : '/static/TerriaMap/wwwroot/build/',
             sourcePrefix: '', // to avoid breaking multi-line string literals by inserting extra tabs.
             globalObject: '(self || window)' // to avoid breaking in web worker (https://github.com/webpack/webpack/issues/6642)
         },

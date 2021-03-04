27654ce48ff60016e67fe965cc91c46a986c5abd
Mohamed Radwan <mohamed.radwan@cartologic.com>
2021-03-04 09:19:16 +0200

    Add map statistics box

--- static/cartoview_terriaJs/src/Dialog/DialogWrapper.jsx
+++ static/cartoview_terriaJs/src/Dialog/DialogWrapper.jsx
@@ -1,16 +1,21 @@
 import { DialogActions, DialogContent, DialogTitle } from "./DialogComponents"
 import AccountCircleIcon from "@material-ui/icons/AccountCircle"
+import Box from "@material-ui/core/Box"
 import Button from "@material-ui/core/Button"
+import Chip from '@material-ui/core/Chip'
 import Dialog from "@material-ui/core/Dialog"
 import Divider from "@material-ui/core/Divider"
 import PropTypes from 'prop-types'
 import React from "react"
+import ShareIcon from "@material-ui/icons/Share"
 import ShortTextIcon from "@material-ui/icons/ShortText"
+import StarIcon from "@material-ui/icons/Star"
 import TodayIcon from "@material-ui/icons/Today"
 import Typography from "@material-ui/core/Typography"
+import VisibilityIcon from "@material-ui/icons/Visibility"
 import { withStyles } from "@material-ui/core/styles"
 
-const styles = () => ({
+const styles = (theme) => ({
     headline: {
         fontWeight: 'bold',
         display: 'flex',
@@ -23,8 +28,29 @@ const styles = () => ({
     divider: {
         width: '100%',
         margin: '10px 0'
-    }
+    },
+    statisticsBoxWrapper: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-evenly'
+    },
+    statisticsBox: {
+        margin: 5
+    },
+    chip: {
+        margin: theme.spacing(0.5),
+        '&:hover': {
+            backgroundColor: '#124e96'
+        }
+    },
 })
+
+const StyleChip = withStyles({
+    root: {
+        color:'#fff',
+        backgroundColor: '#09274b'
+    }
+})(Chip)
 
 const DialogWrapper = ({classes, map, mapDetailsURL, openDialog, onDialogChange}) => {
 
@@ -75,12 +101,36 @@ const DialogWrapper = ({classes, map, mapDetailsURL, openDialog, onDialogChange}
                 </Typography>
 
                 <Divider light className={classes.divider}/>
-                
-                <img
-                    alt="Map Thumbnail"
-                    src={map.thumbnail_url || "/static/cartoview_terriaJs/img/no-img.png"}
-                    title={map.title}
-                />
+                <Box className={classes.statisticsBoxWrapper}>
+                    <img
+                        alt="Map Thumbnail"
+                        src={map.thumbnail_url || "/static/cartoview_terriaJs/img/no-img.png"}
+                        title={map.title}
+                    />
+                    <div className={classes.statisticsBox}>
+                        <StyleChip
+                            className={classes.chip}
+                            color="primary"
+                            icon={<VisibilityIcon/>}
+                            label={map.popular_count}
+                            title="Views count"
+                        />
+                        <StyleChip
+                            className={classes.chip}
+                            color="primary"
+                            icon={<ShareIcon/>}
+                            label={map.share_count}
+                            title="Share count"
+                        />
+                        <StyleChip
+                            className={classes.chip}
+                            color="primary"
+                            icon={<StarIcon/>}
+                            label={map.rating}
+                            title="Rating count"
+                        />
+                    </div>
+                </Box>
             </DialogContent>
             <DialogActions>
                 <Button

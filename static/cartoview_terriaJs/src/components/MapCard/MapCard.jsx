1cac43576b7c76c7c87df19a73af77d1450b1411
Mohamed Radwan <mohamed.radwan@cartologic.com>
2021-03-04 15:24:21 +0200

    Import MapCard styles in a separate file

--- static/cartoview_terriaJs/src/components/MapCard/MapCard.jsx
+++ static/cartoview_terriaJs/src/components/MapCard/MapCard.jsx
@@ -16,46 +16,8 @@ import React from 'react'
 import ShareIcon from '@material-ui/icons/Share'
 import Typography from '@material-ui/core/Typography'
 import copy from 'copy-to-clipboard'
-import {cyan} from '@material-ui/core/colors'
+import styles from './Styles'
 import {withStyles} from '@material-ui/core/styles'
-
-const styles = () => ({
-    card: {
-        width: '100%',
-        minHeight: 290,
-        height: 300,
-        boxShadow: 'none',
-        border: '1px solid #DFE1E5',
-        '&:hover': {
-            border: '1px solid #3BBDD4',
-            transform: 'translateY(-10px)',
-            boxShadow: '0 12px 19px -7px #3BBDD4'
-        }
-    },
-    media: {
-        height: 194,
-        cursor: 'pointer'
-    },
-    avatar: {
-        backgroundColor: cyan[500],
-    },
-    menuItemButton: {
-        color: '#3BBDD4',
-        padding: '1px 10px',
-        '&:hover': {
-            backgroundColor: 'transparent'
-        }
-    },
-    menuItemIcon: {
-        marginLeft: -16
-    },
-    menuItemText: {
-        marginLeft: 16
-    },
-    mapDescribtion: {
-        padding: 10
-    }
-})
 
 class MapCard extends React.Component {
     constructor(props) {

5309b2c6236278d5e55fd95f98f2b022a18fa313
Mohamed Radwan <mohamed.radwan@cartologic.com>
2021-03-08 14:08:56 +0200

    Remove SortBy functionality to add it as a map-settings furtherly

--- static/cartoview_terriaJs/src/components/Navbar/Navbar.jsx
+++ static/cartoview_terriaJs/src/components/Navbar/Navbar.jsx
@@ -1,48 +1,12 @@
-import {
-    Chip,
-    ClickAwayListener,
-    FormControl,
-    FormControlLabel,
-    Grow,
-    Hidden,
-    Paper,
-    Popper,
-    RadioGroup,
-    Toolbar,
-    Typography
-} from '@material-ui/core'
-import React, { useEffect, useRef, useState } from 'react'
+import { Hidden, Toolbar, Typography } from '@material-ui/core'
 import PropTypes from 'prop-types'
-import SortIcon from '@material-ui/icons/Sort'
-import StyledRadio from '../StyledRadio/StyledRadio'
+import React from 'react'
 import ToggleView from './ToggleView'
 import styles from './Styles'
 import terriaLogo from '../../../img/terria-logo.png'
 import { withStyles } from '@material-ui/core/styles'
 
-
-const Navbar = ({ classes, sortMapsBy, handleChange, mapsView, handleChangeMapsView }) => {
-    const [openSortMenu, setOpenSortMenu] = useState(false)
-    const anchorSortRef = useRef(null)
-
-    const handleToggle = () => {
-        setOpenSortMenu((prevOpenSortMenu) => !prevOpenSortMenu)
-    }
-    const handleSortMenuClose = event => {
-        if (anchorSortRef.current && anchorSortRef.current.contains(event.target)) {
-            return
-        }
-        setOpenSortMenu(false)
-    }
-
-    const prevOpenSortMenu = useRef(openSortMenu)
-    useEffect(() => {
-        if (prevOpenSortMenu.current === true && openSortMenu === false) {
-            anchorSortRef.current.focus()
-        }
-        prevOpenSortMenu.current = openSortMenu
-    }, [openSortMenu])
-
+const Navbar = ({ classes, mapsView, handleChangeMapsView }) => {
     return (
         <Toolbar disableGutters>
             <img src={terriaLogo} alt="Terria Maps list" className={classes.media}/>
@@ -50,76 +14,14 @@ const Navbar = ({ classes, sortMapsBy, handleChange, mapsView, handleChangeMapsV
                 Terria Map
             </Typography>
             <Hidden smDown>
-                <ToggleView handleChangeMapsView={handleChangeMapsView} mapsView={mapsView}/>
+                <ToggleView handleChangeMapsView={handleChangeMapsView} mapsView={mapsView} className={classes.toggle}/>
             </Hidden>
-            <Chip
-                icon={<SortIcon className={classes.sortIcon}/>}
-                label="Sort By"
-                className={classes.sortButton}
-                onClick={handleToggle}
-                ref={anchorSortRef}
-            />
-            <Popper
-                open={openSortMenu}
-                anchorEl={anchorSortRef.current}
-                role={undefined}
-                transition
-                disablePortal
-            >
-                {({TransitionProps, placement}) => (
-                    <Grow
-                        {...TransitionProps}
-                        style={{transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom'}}
-                    >
-                        <Paper>
-                            <ClickAwayListener onClickAway={handleSortMenuClose}>
-                                <FormControl component="fieldset">
-                                    <RadioGroup
-                                        aria-label="order by"
-                                        name="order by"
-                                        value={sortMapsBy}
-                                        onChange={handleChange}
-                                        className={classes.radioGroup}
-                                    >
-                                        <FormControlLabel
-                                            value="-date"
-                                            control={<StyledRadio/>}
-                                            label="Most recent"
-                                            className={classes.formControlLabel}
-                                        />
-                                        <FormControlLabel
-                                            value="date"
-                                            control={<StyledRadio/>}
-                                            label="Less recent"
-                                            className={classes.formControlLabel}
-                                        />
-                                        <FormControlLabel
-                                            value="title"
-                                            control={<StyledRadio/>}
-                                            label="A - Z"
-                                            className={classes.formControlLabel}
-                                        />
-                                        <FormControlLabel
-                                            value="-title"
-                                            control={<StyledRadio/>}
-                                            label="Z - A"
-                                            className={classes.formControlLabel}
-                                        />
-                                    </RadioGroup>
-                                </FormControl>
-                            </ClickAwayListener>
-                        </Paper>
-                    </Grow>
-                )}
-            </Popper>
         </Toolbar>
     )
 }
 
 Navbar.propTypes = {
     classes: PropTypes.object.isRequired,
-    sortMapsBy: PropTypes.string.isRequired,
-    handleChange: PropTypes.func.isRequired,
     mapsView: PropTypes.string.isRequired,
     handleChangeMapsView: PropTypes.func.isRequired
 }

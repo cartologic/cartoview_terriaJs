466889719846a43cdac0863e1404445a0bff0292
Mohamed Radwan <mohamed.radwan@cartologic.com>
2021-03-08 20:02:09 +0200

    Add missing translations

--- static/TerriaMap/lib/Views/AboutButton.jsx
+++ static/TerriaMap/lib/Views/AboutButton.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { withTranslation } from "react-i18next";
 import MenuButton from "terriajs/lib/ReactViews/Map/MenuButton";
 
-export default function AboutButton() {
-  return <MenuButton caption="About" href="https://nationalmap.gov.au/about.html" />;
-}
+const AboutButton = () => {
+    const { t } = this.props;
+    return (
+        <MenuButton
+            caption={t("about.caption")}
+            href="https://nationalmap.gov.au/about.html"
+        />
+    );
+};
+
+export default withTranslation()(AboutButton);
